52d7e98d342531805c2db4683e94c6fb60b7c548
Bert Riffelmacher <sotsoguk@gmail.com>
2026-05-11 16:14:00 +0200

    init commit

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {}, // Hier das neue Paket eintragen
+    autoprefixer: {},
+  },
+}
---
+++ src/stadiums.js
@@ -0,0 +1,133 @@
+// export const stadiums = [
+//   {
+//     id: "fcn",
+//     team: "1. FC Nürnberg",
+//     stadium: "Max-Morlock-Stadion",
+//     city: "Nürnberg",
+//     coords: { lat: 49.4262, lng: 11.1226 },
+//     placeId: "ChIJ69-58_pUnEcR3D_L7E-f44U"
+//   },
+//   {
+//     id: "hsv",
+//     team: "Hamburger SV",
+//     stadium: "Volksparkstadion",
+//     city: "Hamburg",
+//     coords: { lat: 53.5872, lng: 9.8987 },
+//     placeId: "ChIJ_682M9SIsUcR7vG_L6O9-4Q"
+//   },
+//   // Hier die restlichen 16 Teams ergänzen...
+// ];
+
+export const stadiums = [
+  {
+    id: "fcn",
+    team: "1. FC Nürnberg",
+    stadium: "Max-Morlock-Stadion",
+    coords: { lat: 49.426389, lng: 11.125556 },
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/1._FC_N%C3%BCrnberg_logo.svg/500px-1._FC_N%C3%BCrnberg_logo.svg.png",
+    matchday: "TBA", 
+    date: "TBA"
+  },
+  {
+    id: "sgf",
+    team: "SpVgg Greuther Fürth",
+    stadium: "Sportpark Ronhof",
+    coords: { lat: 49.483, lng: 10.996 }
+  },
+  {
+    id: "hsv",
+    team: "Hamburger SV",
+    stadium: "Volksparkstadion",
+    coords: { lat: 53.587, lng: 9.898 }
+  },
+  {
+    id: "s04",
+    team: "FC Schalke 04",
+    stadium: "Veltins-Arena",
+    coords: { lat: 51.5545, lng: 7.0675 }
+  },
+  {
+    id: "bsc",
+    team: "Hertha BSC",
+    stadium: "Olympiastadion Berlin",
+    coords: { lat: 52.514722, lng: 13.239444 }
+  },
+  {
+    id: "koe",
+    team: "1. FC Köln",
+    stadium: "RheinEnergieStadion",
+    coords: { lat: 50.933333, lng: 6.875 }
+  },
+  {
+    id: "f95",
+    team: "Fortuna Düsseldorf",
+    stadium: "Merkur Spiel-Arena",
+    coords: { lat: 51.261667, lng: 6.733056 }
+  },
+  {
+    id: "ksc",
+    team: "Karlsruher SC",
+    stadium: "BBBank Wildpark",
+    coords: { lat: 49.02, lng: 8.413 }
+  },
+  {
+    id: "h96",
+    team: "Hannover 96",
+    stadium: "Heinz von Heiden Arena",
+    coords: { lat: 52.36, lng: 9.731111 }
+  },
+  {
+    id: "fck",
+    team: "1. FC Kaiserslautern",
+    stadium: "Fritz-Walter-Stadion",
+    coords: { lat: 49.433611, lng: 7.776111 }
+  },
+  {
+    id: "svd",
+    team: "SV Darmstadt 98",
+    stadium: "Merck-Stadion am Böllenfalltor",
+    coords: { lat: 49.858, lng: 8.675 }
+  },
+  {
+    id: "scp",
+    team: "SC Paderborn 07",
+    stadium: "Home Deluxe Arena",
+    coords: { lat: 51.730556, lng: 8.711389 }
+  },
+  {
+    id: "fcm",
+    team: "1. FC Magdeburg",
+    stadium: "MDCC-Arena",
+    coords: { lat: 52.126, lng: 11.67 }
+  },
+  {
+    id: "ebs",
+    team: "Eintracht Braunschweig",
+    stadium: "Eintracht-Stadion",
+    coords: { lat: 52.290278, lng: 10.521111 }
+  },
+  {
+    id: "sve",
+    team: "SV Elversberg",
+    stadium: "URSAPHARM-Arena",
+    coords: { lat: 49.314, lng: 7.115 }
+  },
+  {
+    id: "prm",
+    team: "Preußen Münster",
+    stadium: "Preußenstadion",
+    coords: { lat: 51.929, lng: 7.623 }
+  },
+  {
+    id: "ssv",
+    team: "SSV Ulm 1846",
+    stadium: "Donaustadion",
+    coords: { lat: 48.406, lng: 10.008 }
+  },
+  {
+    id: "ssvj",
+    team: "SSV Jahn Regensburg",
+    stadium: "Jahnstadion Regensburg",
+    coords: { lat: 48.990278, lng: 12.091111 }
+  }
+];
